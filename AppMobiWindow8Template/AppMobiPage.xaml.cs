e21daf27b3821a0978f50b883730440f358710cb
ryanfiorini <ryanwfiorini@gmail.com>
2012-10-15 13:41:31 -0400

    code clean up

--- AppMobiWindow8Template/AppMobiPage.xaml.cs
+++ AppMobiWindow8Template/AppMobiPage.xaml.cs
@@ -14,7 +14,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace AppMobiWindow8Template
+namespace AppMobiWindows8Template
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
